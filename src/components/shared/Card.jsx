4d3a226dbc294586398c484ffb712b7ef9995f9b
sumeshk1 <sumesh1712@gmail.com>
2025-03-24 13:08:40 +0530

    Add a card component and render using prop's children property

---
+++ src/components/shared/Card.jsx
@@ -0,0 +1,14 @@
+function Card({children, reverse=false}) {
+    console.log(children)
+
+    // conditional class
+  //return <div className={` card ${reverse && 'reverse'}`}>{children}</div>
+
+  // Conditional styling
+  return <div className='card' style={{
+    backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+    color: reverse ?   '#fff'  :  'rgb(0,0,0)'
+   }}>{children}</div>
+}
+
+export default Card
